818a984103ff5ffcb4daa63cb08021a2c5f51d59
NoxYeah <NoxYeah@users.noreply.github.com>
2021-02-08 23:29:59 -0600

    ADD:PokeMon API to slotMachine

--- js/main.js
+++ js/main.js
@@ -9,12 +9,39 @@ const dom = {
     spinResults: document.querySelector('#spinResults'),
     spinResultsContainer: document.querySelector('#spinResultsContainer'),
     numInput: document.querySelector('numInput'),
-
-    changeMoney: function() {
-        this.numInput.addEventListener('')
-    }
-
 }
+
+// fetch Pokemon Api that are in pokemon Arr
+function pokeMonData () {
+    // loop through array
+    for(let i = 0;i < 10; i++) {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${Math.trunc(Math.random() * 151)}`)
+            .then(res => res.json())
+            .then(data => {
+                //data.sprites.front_shiny
+                pokeMonArr.push(new PokeMonObj(data.name,data.sprites.front_shiny))
+                // pokeMonImgs.push((data.sprites.front_shiny))
+            })
+            .catch(err => {
+                console.log(`${err}`)
+            })
+
+    }
+    
+}
+
+pokeMonData()
+
+function PokeMonObj(name,img) {
+    this.name = name
+    this.img = img
+}
+
+
+let pokeMonArr = []
+console.log(pokeMonArr)
+// console.log(pokeMonImgs)
+
 
 class SlotValues {
     constructor(leftValue,midValue,rightValue) {
@@ -24,21 +51,26 @@ class SlotValues {
     }
 
     slotMachineLogic() {
-        if (this.leftValue !== this.midValue && this.leftValue !== this.rightvalue ) {
+        // checks all 3 values
+        if (this.leftValue === this.midValue && this.midValue === this.rightValue) {
+            this.moneyLogic(100)
+            console.log(`JACKPOT!`)
+        } //checks if left matches midd OR middle matches Right
+        else if (this.leftValue === this.midValue || this.midValue === this.rightValue ) {
+            this.moneyLogic(40)
+            console.log(`you win +40`)
+        }
+         // Left value matches right
+         else if (this.leftValue === this.rightValue) {
+            this.moneyLogic(20)
+            console.log(`You win $20`)
+        } else {
             this.moneyLogic(0)
             dom.valueWon.innerHTML = 0
             console.log(`You lost`)
-        } else if (this.leftValue === this.rightValue) {
-            this.moneyLogic(20)
-            console.log(`you win +20`)
-        } else if (this.leftValue === this.midValue || this.midValue === this.rightValue) {
-            this.moneyLogic(40)
-            console.log(`you win +40`)
-        } else if (this.leftValue === this.midValue && this.midValue === this.rightValue) {
-            this.moneyLogic(100)
-            console.log(`JACKPOT!`)
         }
-    }
+     }
+
     moneyLogic(value) {
         if( value === 0 ) {
             dom.spinResults.innerHTML = `You Lost!`
@@ -67,11 +99,23 @@ let slotMachineValues
 
 
 function offToTheRaces(){
-    dom.leftIcons.innerHTML = slotIcons[randomNumber(slotIcons.length)]
-    dom.midIcons.innerHTML = slotIcons[randomNumber(slotIcons.length)]
-    dom.rightIcons.innerHTML = slotIcons[randomNumber(slotIcons.length)]
-    slotMachineValues = new SlotValues(dom.leftIcons.innerHTML,dom.midIcons.innerHTML,dom.rightIcons.innerHTML)
+    
+    dom.leftIcons.src = pokeMonArr[randomNumber(pokeMonArr.length)].img
+    dom.midIcons.src = pokeMonArr[randomNumber(pokeMonArr.length)].img
+    dom.rightIcons.src = pokeMonArr[randomNumber(pokeMonArr.length)].img
+
+    slotMachineValues = new SlotValues(dom.leftIcons.src,dom.midIcons.src,dom.rightIcons.src)
     slotMachineValues.slotMachineLogic()
+
+    console.log(typeof slotMachineValues.leftValue,typeof slotMachineValues.midValue,typeof slotMachineValues.rightValue)
+    console.log(slotMachineValues)
+
+                    //working version below
+    // dom.leftIcons.innerHTML = slotIcons[randomNumber(slotIcons.length)]
+    // dom.midIcons.innerHTML = slotIcons[randomNumber(slotIcons.length)]
+    // dom.rightIcons.innerHTML = slotIcons[randomNumber(slotIcons.length)]
+    // slotMachineValues = new SlotValues(dom.leftIcons.innerHTML,dom.midIcons.innerHTML,dom.rightIcons.innerHTML)
+    // slotMachineValues.slotMachineLogic()
   
     // console.log(slotIcons[randomNum1],slotIcons[randomNum2],slotIcons[randomNum3])
     // console.log(randomNumber(slotIcons.length))
